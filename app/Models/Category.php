be73b1d144e92cc70e185dcbb1d6239c53297c76
Ricardo Duarte <2240879@my.ipleiria.pt>
2026-05-27 17:33:07 +0100

    Modelos

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,11 +2,21 @@
 
 namespace App\Models;
 
+use App\Models\Traits\HasCommonFillable\HasSoftDeleteOnly;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Traits\HasSoftDeleteOnly;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Table;
+use App\Models\Tshirt_image;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 #[Fillable(['name', 'image_url'])]
 class Category extends Model
 {
     use HasSoftDeleteOnly;
+
+    //Relação Category-Tshirt_Images
+    public function tshirt_Images(): HasMany
+    {
+        return $this->hasMany(Tshirt_image::class, 'category_id', 'id');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,26 +2,21 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use App\Models\Traits\HasCommonFillable\HasFullTimestamps;
-use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use App\Models\Customer;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 #[Fillable(['name', 'email', 'password', 'user_type', 'gender', 'blocked', 'photo_url'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
-    /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable, HasFullTimestamps;
+    use HasFactory, Notifiable, SoftDeletes;
 
-    /**
-     * Get the attributes that should be cast.
+     /* Get the attributes that should be cast.
      *
      * @return array<string, string>
      */
@@ -30,9 +25,11 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'blocked' => 'boolean',
         ];
     }
 
+    //Relação Customer-user
     public function customer(): HasOne
     {
         return $this->hasOne(Customer::class, 'id', 'id');
