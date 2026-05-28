43f83855d3598cc7f5a7879a11980a2d08d57fd7
João Soares <2240859@my.ipleiria.pt>
2026-05-28 13:57:40 +0100

    main

--- resources/views/partials/main-content-headings.blade.php
+++ resources/views/partials/main-content-headings.blade.php
@@ -2,8 +2,5 @@
     <flux:heading size="xl" level="1">
         {{ $heading ?? ($title ?? 'IP Leiria')}}
     </flux:heading>
-    <flux:subheading size="lg" class="mb-6">
-        {{ $subheading ?? 'Department of Computer Engineering' }}
-    </flux:subheading>
     <flux:separator variant="subtle" />
 </div>
--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CatalogController;
+use App\Http\Controllers\CustomizationController;
+use App\Http\Controllers\CartController;
+
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/catalog', [CatalogController::class, 'index'])->name('catalog.index');
+Route::get('/customization', [CustomizationController::class, 'index'])->name('customization.index');
+Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
